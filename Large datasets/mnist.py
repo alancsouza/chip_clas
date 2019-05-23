51a9eb94b808c11129105afbe23483a1172f0177
Alan <alancsouza04@gmain.com>
2019-05-23 20:02:36 -0300

    small mnist dataset

--- Large datasets/mnist.py
+++ Large datasets/mnist.py
@@ -10,39 +10,53 @@ data_name = "mnist"
 
 (X_train, y_train), (X_test, y_test) = mnist.load_data()
 
-# Reshaping the data
-X_train = X_train.reshape(60000, 784)
-X_test = X_test.reshape(10000, 784)
-
-# Small data due to memory error
-X_train = X_train[:10000,:]
-X_test = X_test[:1000,:]
-
 # re-scale the image data to values between (0.0,1.0]
 X_train = X_train.astype('float32') / 255.
 X_test = X_test.astype('float32') / 255.
 
-# Converting to binary classification problem, odd digits or even digits
+# Reshaping the data
+X_train = X_train.reshape(60000, 784)
+X_test = X_test.reshape(10000, 784)
+
+# making binary classification problem: 2 vs rest
 y_train_binary = (y_train % 2 == 0).astype(np.int)
 y_test_binary = (y_test % 2 == 0).astype(np.int)
 
-# Converting the numpy array to pandas data frame
-X_train = pd.DataFrame(X_train)
-X_test = pd.DataFrame(X_test)
+# getting a smaller subset of the mnist dataset
+Data_train = np.c_[X_train, y_train_binary]
+Data_test = np.c_[X_test, y_test_binary]
+Data_mnist = np.concatenate((Data_train, Data_test), axis = 0)
+Data_mnist = pd.DataFrame(Data_mnist)
 
-y_train_binary = pd.DataFrame(y_train_binary)
-y_test_binary = pd.DataFrame(y_test_binary)
+# separating the lables
+c1 = Data_mnist[Data_mnist.iloc[:,-1] ==  1]
+c2 = Data_mnist[Data_mnist.iloc[:,-1] == 0]
+
+c1_small = c1.sample(n = 5000, random_state = 1)
+c2_small = c2.sample(n = 5000, random_state = 1)
+
+Small_mnist = pd.concat([c1_small, c2_small])
+
+# shuffle data
+Small_mnist = Small_mnist.sample(frac = 1 )
+
+X = Small_mnist.iloc[:,:-1]
+y = Small_mnist.iloc[:,-1]
+y[y==0] = -1
 
 # Filtering data:
-X_train_new, y_train_new = remove_noise(X_train, y_train_binary)
+X_new, y_new = remove_noise(X, y)
+
+# Train and Test split
+X_train, X_test, y_train, y_test = train_test_split(X_new, y_new, test_size=0.2, random_state=42)
 
 # Comparing methods:
-method = ["nn_clas", "parallel", "pseudo_support_edges"]
+method = ["parallel", "nn_clas", "pseudo_support_edges"]
 
 print("Dataset: {}".format(data_name))
 
 for model in method:
-    y_hat, y_test, result, runtime = chip_clas_new(X_train_new, X_test, y_train_new, y_test_binary, method = model)
+    y_hat, y_test, result, runtime = chip_clas_new(X_train, X_test, y_train, y_test, method = model)
 
     print(" \n Method: {0} \n AUC: {1:.4f} \n Runtime: {2:.4f} \n".format(model, result, runtime))
 
