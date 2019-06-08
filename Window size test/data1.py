041fccf60189b59ee09e4d0c4756802b7b0d1190
Alan <alancsouza04@gmain.com>
2019-06-08 15:12:30 -0300

    windows size test

--- Window size test/data1.py
+++ Window size test/data1.py
@@ -4,8 +4,13 @@
 data1 = Banknote Auth.
 """
 
-import chip_clas_new
+from chip_clas_new import chip_clas_new
 import statistics
+from functions import remove_noise
+from sklearn.model_selection import train_test_split, KFold
+from sklearn.preprocessing import MinMaxScaler
+import numpy as np
+import pandas as pd
 
 data_name = "Banknote Auth."
 
@@ -14,7 +19,7 @@ data = pd.read_csv(url, header = None)
 
 X = data.iloc[:,:-1]
 
-min_max_scaler = preprocessing.MinMaxScaler(feature_range=(-1, 1)) # Normalizing data between -1 and 1
+min_max_scaler = MinMaxScaler(feature_range=(-1, 1)) # Normalizing data between -1 and 1
 X = pd.DataFrame(min_max_scaler.fit_transform(X))
 y = data.iloc[:,-1].copy()
 y[y == 0] = -1
@@ -29,17 +34,16 @@ kf = KFold(n_splits=k, shuffle = True, random_state = 1)
 results = []
 runtime = []
 
-split_size = 10
+split = 10
 
 for train_index, test_index in kf.split(X_new):
-    start = time.time()   
-
+    
     X_train, X_test = X_new.iloc[train_index], X_new.iloc[test_index]
     y_train, y_test = y_new.iloc[train_index], y_new.iloc[test_index]
 
-    y_hat, y_test, result, runtime = chip_clas_new(X_train, X_test, y_train, y_test, method = "parallel" , split_size)
+    y_hat, y_test, result, runtime = chip_clas_new(X_train, X_test, y_train, y_test, method = "parallel", split_size = split)
 
-    print(" \n Method: {0} \n AUC: {1:.4f} \n Runtime: {2:.4f} \n".format(model, result, runtime))
+    print("\n AUC: {1:.4f} \n Runtime: {2:.4f} \n".format(result, runtime))
 
     runtime.append(runtime)
     results.append(result)
@@ -50,10 +54,8 @@ mean_runtime = sum(runtime)/len(runtime)
 std = statistics.stdev(results)
 
 f = open("results_window_size.txt", "a+")
-f.write("\n\nSplit_size: %s \n" % split_size)
+f.write("\n\nSplit_size: %d \n" % split)
 f.write("AUC: %d \n" % mean_AUC)
 f.write("Std Deviation: %d \n" % std)
 f.write("Runtime: %d \n" % mean_runtime)
 f.close()
-
-
