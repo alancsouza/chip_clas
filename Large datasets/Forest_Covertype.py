55f1608fff50ffb3623b41d24ba73630fda4ae70
Alan <alancsouza04@gmain.com>
2019-06-15 14:46:11 -0300

    Forest data

--- Large datasets/Forest_Covertype.py
+++ Large datasets/Forest_Covertype.py
@@ -23,18 +23,19 @@ min_max_scaler = preprocessing.MinMaxScaler()
 X = pd.DataFrame(min_max_scaler.fit_transform(X))
 
 # Filtering data:
-X_new, y_new = remove_noise(X, y_binary)
+#X_new, y_new = remove_noise(X, y_binary)
 
 # Train and Test split
-X_train, X_test, y_train, y_test = train_test_split(X_new, y_new, test_size=0.2, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(X, y_binary, test_size=0.2, random_state=42)
 
 # Comparing methods:
 method = ["nn_clas", "parallel", "pseudo_support_edges"]
 
 print("Dataset: {}".format(data_name))
 
+
 for model in method:
-    y_hat, y_test, result, runtime = chip_clas_new(X_train, X_test, y_train, y_test, method = model)
+    y_hat, y_test, result, runtime = chip_clas_new(X_train, X_test, y_train, y_test, method = model, split_size = 6)
 
     print(" \n Method: {0} \n AUC: {1:.4f} \n Runtime: {2:.4f} \n".format(model, result, runtime))
 
